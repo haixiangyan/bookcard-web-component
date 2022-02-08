51bbe91517a42bfa04c389625cdfc3a4a35c5a05
haixiangyan <haixiang6123@gmail.com>
2022-02-08 23:15:43 +0800

    feat: 使用模板

--- BookCard.js
+++ BookCard.js
@@ -5,27 +5,8 @@ class BookCard extends HTMLElement {
   }
 
   render() {
-    const container = document.createElement('div')
-
-    const image = document.createElement('img')
-    image.className = 'image'
-    image.src = this.getAttribute('image')
-
-    const title = document.createElement('p')
-    title.className = 'title'
-    title.textContent = this.getAttribute('title')
-
-    const desc = document.createElement('p')
-    desc.className = 'desc'
-    desc.textContent = this.getAttribute('desc')
-
-    const price = document.createElement('p')
-    price.className = 'price'
-    price.textContent = this.getAttribute('price')
-
-    container.append(image, title, desc, price)
-
-    this.append(container)
+    const template = document.getElementById('book-card-template').content
+    this.appendChild(template.cloneNode(true))
   }
 }
 
